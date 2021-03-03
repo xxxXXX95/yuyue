6bdb18f8f6283e515dd3df1d50835211018291f3
superq <13815865+meooxx@users.noreply.github.com>
2021-03-03 21:50:43 +0800

    Feat: 增加eslint & output 输出当前时间

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const cluster = require('cluster');
 const path = require('path');
-const filePath = path.join(__dirname, 'config.js');
 const fs = require('fs');
+const dayjs = require('./dayjs.min.js');
+
+const filePath = path.join(__dirname, 'config.js');
 try {
   fs.accessSync(filePath, fs.constants.F_OK);
 } catch (e) {
@@ -13,6 +15,7 @@ try {
 const { login, submitOrderProcess } = require('./jobs');
 const { pool, forceLogin } = require('./tasks-pool');
 const config = require('./config');
+
 if (!config.eid || !config.fp) {
   console.log('请在 config.js 按备注填写 `eid` 和  `fp`');
   process.exit();
@@ -29,22 +32,26 @@ if (cluster.isWorker) {
     return login(item.forceLogin);
   };
   const setupWork = item => {
-    const { date, skuId, areaId = config.areaId, forceKO = false } = item;
+    const {
+      date,
+      skuId,
+      areaId = config.areaId,
+      forceKO = false,
+      ...rest
+    } = item;
     const expectedDate = new Date(item.date);
     // 任务进程
     console.log(
       'process.worker:',
       process.pid,
       '时间:',
-      `${expectedDate.toLocaleString('en-US', {
-        hour12: false,
-      })}.${expectedDate.getMilliseconds()}`,
+      dayjs(expectedDate).format('YYYY-MM-DD HH:mm:ss.SSS'),
       'sku',
       item.skuId
     );
-    submitOrderProcess(date, skuId, areaId, forceKO);
+    submitOrderProcess(date, skuId, areaId, { forceKO, ...rest });
   };
-  process.on('unhandledRejection', (reason, promise) => {
+  process.on('unhandledRejection', reason => {
     console.log(reason);
   });
   cluster.worker.on('message', async job => {
@@ -63,16 +70,17 @@ if (cluster.isWorker) {
   cluster.on('message', (_, message) => {
     // 登陆流程
     if (message.doneWork === 'login') {
+      // eslint-disable-next-line global-require
       require('./codeInfo');
       // 登陆完成后
-      for (i = 0; i < pool.length; i++) {
+      for (let i = 0; i < pool.length; i++) {
         const item = pool[i];
         cluster.fork().send({ ...item, type: 'task' });
       }
     }
     // fork 秒杀流程
     if (message.doneWork === 'forkKO') {
-      for (i = 0; i < message.items.length; i++) {
+      for (let i = 0; i < message.items.length; i++) {
         const item = message.items[i];
         cluster.fork().send({ ...item, type: 'task', forceKO: true });
       }
