f501a9e0286858eace867ca80c012e1b55f0eaeb
superq <13815865+meooxx@users.noreply.github.com>
2021-03-21 21:58:04 +0800

    Feat: a new Api of time server (#20)
    * Feat: a new Api of time server
    * Fix: windows下不实用stdout.write 输出时间

--- timer.js
+++ timer.js
@@ -4,7 +4,7 @@ const dayjs = require('./dayjs.min.js');
 
 const getDiffTime = async () => {
   const old = Date.now();
-  const res = await fetch('https://a.jd.com//ajax/queryServerData.html', {
+  const res = await fetch('https://api.m.jd.com/client.action?functionId=queryMaterialProducts&client=wh5', {
     headers: {
       // 瞎鸡儿填写一个 UA
       'User-Agent': config.userAgent || 'chrome 81.0',
@@ -12,29 +12,27 @@ const getDiffTime = async () => {
   });
   const now = Date.now();
   const json = await res.json();
-  return Date.now() - (now - old) / 2 - json.serverTime;
+  return Date.now() - (now - old) / 2 - json.currentTime2;
 };
 
 module.exports = async (d, fn, im = false) => {
   // Should excute immediately
   if (im) return Promise.resolve(fn());
-  // const diffTime = await Promise.race([
-  //   getDiffTime(),
-  //   getDiffTime(),
-  //   getDiffTime(),
-  //   getDiffTime(),
-  // ]);
-  // console.log(
-  //   '多次请求中最快的一次与服务器时间差,',
-  //   'now - 请求往返/2 - jd服务器时间=',
-  //   diffTime
-  // );
-  console.log('jd时间服务器挂壁, 请自行根据以往误差往前调抢购时间');
+  const diffTime = await Promise.race([
+    getDiffTime(),
+    getDiffTime(),
+    getDiffTime(),
+    getDiffTime(),
+  ]);
+  console.log(
+    '多次请求中最快的一次与服务器时间差,',
+    'now - 请求往返/2 - jd服务器时间=',
+    diffTime
+  );
   return new Promise(r => {
     console.log('等待时间到达:', dayjs(d).format('YYYY-MM-DD HH:mm:ss.SSS'));
     while (true) {
-      // if (Date.now() - diffTime >= d) {
-      if (Date.now() >= d) {
+      if (Date.now() - diffTime >= d) {
         r(fn());
         break;
       }
